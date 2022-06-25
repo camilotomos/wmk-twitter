7dfcb875c868361f3c3f53323b7c2fafe2821c4f
CamiloTomos <camilotomos@gmail.com>
2022-06-25 18:20:39 -0500

    Primer commit - logo ixon y doc base

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+const Title = () => <h1>wmk - twitter</h1>
+const Content = () => <p>Welcome to WebMaker04</p>
+const Footer = () => <footer>all rights reserved - power by camilotomos</footer>
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Title/>
+      <Content/>
+      <Footer/>
+    </>
   );
 }
 
